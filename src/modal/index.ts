2eb18cb73004fad67f5eda254d45800a53297a0f
akkaradej <akkaradej14@gmail.com>
2018-01-04 23:15:24 +0700

    export ModalComponent

--- src/modal/index.ts
+++ src/modal/index.ts
@@ -1,2 +1,3 @@
+export { ModalComponent } from './modal.component';
 export { ModalContent } from './modal-content';
 export { ModalModule } from './modal.module';
--- src/soft-ngx.ts
+++ src/soft-ngx.ts
@@ -2,7 +2,7 @@
 export { ApiClientConfig, ApiClientModule, ApiClientService, AuthHelperService, 
   Auth, AuthService, StorageService } from './api-client/index';
 export { BusyModule, BusyConfig } from './busy/index';
-export { ModalModule, ModalContent } from './modal/index';
+export { ModalModule, ModalComponent, ModalContent } from './modal/index';
 export { ModelHelperModule } from './model-helper/index';
 export { PipeExtensionModule } from './pipe-extension/index';
 export { PopupModule, PopupService } from './popup/index';
